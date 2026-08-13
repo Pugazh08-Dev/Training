b316c6dcdd0ca384d4cbe8c8d165c146219007d3
Pugazhendhi Murugesan <Pugazhendhi.Murugesan@trumpf.com>
2026-08-13 10:05:09 +0530

    A04 : Finding Occurences

---
+++ A04/Program.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch- July 2026. 
+// Copyright (c) Metamation India. 
+// ------------------------------------------------------------------------------------------------- 
+
+// Program.cs 
+// Write a program to find the occurences of a given character in a Words.txt file.
+// The program should read the file, count the number of times the specified character appears, and display the result to the user.	 
+// ------------------------------------------------------------------------------------------------ 
+
+var charCount = new Dictionary<char, int> {
+   ['U'] = 0,
+   ['X'] = 0,
+   ['A'] = 0,
+   ['L'] = 0,
+   ['T'] = 0,
+   ['N'] = 0,
+   ['E'] = 0
+};
+
+foreach(var line in File.ReadLines("C:\\Users\\murugesanpu\\Downloads\\words 1.txt")) {
+   foreach(var c in line) {
+      if (charCount.ContainsKey (c)) {
+         charCount[c]++;
+      }
+   }
+}
+
+Console.WriteLine("Here, is the count of the characters in the file:");
+foreach (var kvp in charCount) {
+   Console.WriteLine ($"Character '{kvp.Key}': {kvp.Value} occurrences");
+}
